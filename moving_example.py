f876970e3308dd090c8e5173b9e5e70506e717e2
nobody <148004508+nobody1889@users.noreply.github.com>
2024-06-12 07:32:21 -0400

    Update moving_example.py

--- moving_example.py
+++ moving_example.py
@@ -1,11 +1,12 @@
 from file_reader import FileReader
 
 types: list[str] = input('entre file type : \n').split(' ')  # client input as list
-search = FileReader(types)  # create the instance
+search = FileReader()  # create the instance
 start_from = input("ENTER THE START :(don't enter ~ for home)\n")
 destination = input("ENTER THE DESTINATION :\n")
-search.move_files(start_from=start_from, destination=destination)  # give the start point and end
-print('searched files :  ', search.checked_files_number)
-for file in search.checked_files_list:
-    print(file)
-
+search.move_files(parameters_list=types, start_from=start_from, destination=destination)  # give the start point and end
+size = len(search.parameters.keys())
+print('searched files :  ', size)
+for lists in search.parameters.values():
+    for file in lists:
+        print(file)

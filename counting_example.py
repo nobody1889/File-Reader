f82e3f7a8ec6d4af523c9b7a5f4928ab5a16f90e
nobody <148004508+nobody1889@users.noreply.github.com>
2024-06-12 07:31:38 -0400

    Update counting_example.py

--- counting_example.py
+++ counting_example.py
@@ -1,14 +1,13 @@
 from file_reader import FileReader
 
-types: list[str] = input('entre file type : \n').split(' ')  # client input as list
-search = FileReader(types)  # create the instance
+types: list[str] = input('entre file types or names : \n').split(' ')  # client input as list
+search = FileReader()  # create the instance
 
-mylist = search.count_files(start_from=input("ENTER THE START :(don't enter ~ for home)\n"))  # give the start point
+mylist, d = search.count_files(types, start_from=input("ENTER THE START :(don't enter ~ for home)\n"))
 
 # show our output
 for key in mylist:
-    print(key, mylist[key])
+    print(key + ' : ', mylist[key])
 
-# print(search.checked_files_list)
-print('searched files :  ', search.checked_files_number)
-print(search.checked_files_list)
+num, d_list = d
+print('number of denied files :  ', num)
